7820ed65d4ce3e2f2e832d6471a617d6e5201ec5
kevingoot <37970252+kevingoot@users.noreply.github.com>
2026-06-21 18:59:47 -0400

    Refactor prospect data structure and UI components

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 from datetime import datetime
 
 st.set_page_config(page_title="MLB Prospect Analyzer", page_icon="⚾", layout="wide")
@@ -68,8 +69,8 @@ PROSPECTS = {
            {"player":"Samuel Basallo","pos":"C","score":89,"call_up":"High","bowman":"$25-80","topps":"$80-230"},
            {"player":"Coby Mayo","pos":"3B","score":86,"call_up":"Medium","bowman":"$15-50","topps":"$50-150"}],
     "BOS":[{"player":"Roman Anthony","pos":"OF","score":95,"call_up":"High","bowman":"$60-200","topps":"$200-600"},
-           {"player":"Kyle Teel","pos":"C","score":87,"call_up":"High","bowman":"$20-65","topps":"$65-200"},
-           {"player":"Marcelo Mayer","pos":"SS","score":91,"call_up":"High","bowman":"$35-110","topps":"$110-320"}],
+           {"player":"Marcelo Mayer","pos":"SS","score":91,"call_up":"High","bowman":"$35-110","topps":"$110-320"},
+           {"player":"Kyle Teel","pos":"C","score":87,"call_up":"High","bowman":"$20-65","topps":"$65-200"}],
     "CHC":[{"player":"Matt Shaw","pos":"SS","score":85,"call_up":"Medium","bowman":"$15-50","topps":"$50-150"},
            {"player":"Kevin Made","pos":"SS","score":82,"call_up":"Medium","bowman":"$10-35","topps":"$30-90"},
            {"player":"Owen Caissie","pos":"OF","score":81,"call_up":"Low","bowman":"$8-30","topps":"$25-80"}],
@@ -83,23 +84,23 @@ PROSPECTS = {
            {"player":"Chase DeLauter","pos":"OF","score":83,"call_up":"Medium","bowman":"$10-35","topps":"$30-90"},
            {"player":"Tanner Burns","pos":"RHP","score":80,"call_up":"Low","bowman":"$8-25","topps":"$20-60"}],
     "COL":[{"player":"Chase Dollander","pos":"RHP","score":90,"call_up":"High","bowman":"$30-90","topps":"$90-270"},
-           {"player":"Kyle Freeland Jr.","pos":"LHP","score":82,"call_up":"Medium","bowman":"$8-30","topps":"$25-75"},
-           {"player":"Adael Amador","pos":"SS","score":84,"call_up":"Medium","bowman":"$12-40","topps":"$35-100"}],
+           {"player":"Adael Amador","pos":"SS","score":84,"call_up":"Medium","bowman":"$12-40","topps":"$35-100"},
+           {"player":"Kyle Freeland Jr.","pos":"LHP","score":82,"call_up":"Medium","bowman":"$8-30","topps":"$25-75"}],
     "DET":[{"player":"Jackson Jobe","pos":"RHP","score":91,"call_up":"High","bowman":"$35-110","topps":"$110-320"},
            {"player":"Colt Keith","pos":"2B","score":87,"call_up":"High","bowman":"$20-65","topps":"$65-190"},
            {"player":"Ty Madden","pos":"RHP","score":82,"call_up":"Medium","bowman":"$8-30","topps":"$25-80"}],
     "HOU":[{"player":"Brice Matthews","pos":"SS","score":84,"call_up":"Medium","bowman":"$12-40","topps":"$40-120"},
-           {"player":"Colin Barber","pos":"OF","score":81,"call_up":"Low","bowman":"$8-25","topps":"$20-60"},
-           {"player":"Chayce McDermott","pos":"RHP","score":83,"call_up":"Medium","bowman":"$10-35","topps":"$30-90"}],
+           {"player":"Chayce McDermott","pos":"RHP","score":83,"call_up":"Medium","bowman":"$10-35","topps":"$30-90"},
+           {"player":"Colin Barber","pos":"OF","score":81,"call_up":"Low","bowman":"$8-25","topps":"$20-60"}],
     "KC": [{"player":"Jac Caglianone","pos":"1B/LHP","score":90,"call_up":"High","bowman":"$30-90","topps":"$90-270"},
-           {"player":"Gavin Cross","pos":"OF","score":85,"call_up":"Medium","bowman":"$12-40","topps":"$40-120"},
-           {"player":"Blake Mitchell","pos":"SS","score":87,"call_up":"High","bowman":"$20-65","topps":"$65-190"}],
+           {"player":"Blake Mitchell","pos":"SS","score":87,"call_up":"High","bowman":"$20-65","topps":"$65-190"},
+           {"player":"Gavin Cross","pos":"OF","score":85,"call_up":"Medium","bowman":"$12-40","topps":"$40-120"}],
     "LAA":[{"player":"Nolan Schanuel","pos":"1B","score":85,"call_up":"Medium","bowman":"$12-40","topps":"$40-120"},
-           {"player":"Caden Dana","pos":"RHP","score":83,"call_up":"Medium","bowman":"$10-35","topps":"$30-90"},
-           {"player":"Werner Blakely","pos":"SS","score":84,"call_up":"Medium","bowman":"$12-40","topps":"$35-105"}],
+           {"player":"Werner Blakely","pos":"SS","score":84,"call_up":"Medium","bowman":"$12-40","topps":"$35-105"},
+           {"player":"Caden Dana","pos":"RHP","score":83,"call_up":"Medium","bowman":"$10-35","topps":"$30-90"}],
     "LAD":[{"player":"Dalton Rushing","pos":"C","score":89,"call_up":"High","bowman":"$25-80","topps":"$80-230"},
-           {"player":"Tommy Edman","pos":"SS","score":85,"call_up":"Medium","bowman":"$10-35","topps":"$35-100"},
-           {"player":"Nick Frasso","pos":"RHP","score":84,"call_up":"Medium","bowman":"$10-35","topps":"$30-90"}],
+           {"player":"Nick Frasso","pos":"RHP","score":84,"call_up":"Medium","bowman":"$10-35","topps":"$30-90"},
+           {"player":"Tommy Edman","pos":"SS","score":85,"call_up":"Medium","bowman":"$10-35","topps":"$35-100"}],
     "MIA":[{"player":"Noble Meyer","pos":"RHP","score":91,"call_up":"High","bowman":"$35-110","topps":"$110-320"},
            {"player":"Jacob Berry","pos":"3B","score":85,"call_up":"Medium","bowman":"$12-40","topps":"$40-120"},
            {"player":"Karson Ligon","pos":"RHP","score":82,"call_up":"Medium","bowman":"$8-30","topps":"$25-80"}],
@@ -107,11 +108,11 @@ PROSPECTS = {
            {"player":"Sal Frelick","pos":"OF","score":85,"call_up":"Medium","bowman":"$12-40","topps":"$40-120"},
            {"player":"Joey Wiemer","pos":"OF","score":82,"call_up":"Medium","bowman":"$8-30","topps":"$25-80"}],
     "MIN":[{"player":"Walker Jenkins","pos":"OF","score":93,"call_up":"High","bowman":"$45-140","topps":"$140-420"},
-           {"player":"Hurston Waldrep","pos":"RHP","score":84,"call_up":"Medium","bowman":"$10-35","topps":"$35-100"},
-           {"player":"Emmanuel Rodriguez","pos":"OF","score":86,"call_up":"Medium","bowman":"$15-50","topps":"$50-150"}],
+           {"player":"Emmanuel Rodriguez","pos":"OF","score":86,"call_up":"Medium","bowman":"$15-50","topps":"$50-150"},
+           {"player":"Hurston Waldrep","pos":"RHP","score":84,"call_up":"Medium","bowman":"$10-35","topps":"$35-100"}],
     "NYM":[{"player":"Kevin Parada","pos":"C","score":88,"call_up":"High","bowman":"$20-65","topps":"$65-200"},
-           {"player":"Luisangel Acuna","pos":"SS","score":85,"call_up":"Medium","bowman":"$12-40","topps":"$40-120"},
-           {"player":"Jett Williams","pos":"SS","score":87,"call_up":"High","bowman":"$20-65","topps":"$65-190"}],
+           {"player":"Jett Williams","pos":"SS","score":87,"call_up":"High","bowman":"$20-65","topps":"$65-190"},
+           {"player":"Luisangel Acuna","pos":"SS","score":85,"call_up":"Medium","bowman":"$12-40","topps":"$40-120"}],
     "NYY":[{"player":"Jasson Dominguez","pos":"OF","score":90,"call_up":"High","bowman":"$30-90","topps":"$90-270"},
            {"player":"Spencer Jones","pos":"OF","score":87,"call_up":"High","bowman":"$20-65","topps":"$65-190"},
            {"player":"George Lombard Jr.","pos":"OF","score":84,"call_up":"Medium","bowman":"$10-35","topps":"$35-100"}],
@@ -119,26 +120,26 @@ PROSPECTS = {
            {"player":"Zack Gelof","pos":"2B","score":85,"call_up":"Medium","bowman":"$12-40","topps":"$40-120"},
            {"player":"Max Muncy","pos":"SS","score":83,"call_up":"Medium","bowman":"$10-35","topps":"$30-90"}],
     "PHI":[{"player":"Andrew Painter","pos":"RHP","score":90,"call_up":"High","bowman":"$30-90","topps":"$90-270"},
-           {"player":"Mick Abel","pos":"RHP","score":86,"call_up":"Medium","bowman":"$15-50","topps":"$50-150"},
-           {"player":"Aidan Miller","pos":"SS","score":88,"call_up":"High","bowman":"$20-65","topps":"$65-200"}],
+           {"player":"Aidan Miller","pos":"SS","score":88,"call_up":"High","bowman":"$20-65","topps":"$65-200"},
+           {"player":"Mick Abel","pos":"RHP","score":86,"call_up":"Medium","bowman":"$15-50","topps":"$50-150"}],
     "PIT":[{"player":"Paul Skenes","pos":"RHP","score":98,"call_up":"High","bowman":"$100-350","topps":"$350-1000"},
            {"player":"Termarr Johnson","pos":"2B","score":87,"call_up":"High","bowman":"$20-65","topps":"$65-190"},
            {"player":"Endy Rodriguez","pos":"C","score":85,"call_up":"Medium","bowman":"$12-40","topps":"$40-120"}],
     "SD": [{"player":"Ethan Salas","pos":"C","score":93,"call_up":"High","bowman":"$45-140","topps":"$140-420"},
-           {"player":"Samuel Zavala","pos":"OF","score":88,"call_up":"High","bowman":"$20-65","topps":"$65-200"},
-           {"player":"Robby Snelling","pos":"LHP","score":89,"call_up":"High","bowman":"$25-80","topps":"$80-230"}],
+           {"player":"Robby Snelling","pos":"LHP","score":89,"call_up":"High","bowman":"$25-80","topps":"$80-230"},
+           {"player":"Samuel Zavala","pos":"OF","score":88,"call_up":"High","bowman":"$20-65","topps":"$65-200"}],
     "SF": [{"player":"Bryce Eldridge","pos":"1B/RHP","score":89,"call_up":"High","bowman":"$25-80","topps":"$80-230"},
            {"player":"Marco Luciano","pos":"SS","score":85,"call_up":"Medium","bowman":"$12-40","topps":"$40-120"},
            {"player":"Kyle Harrison","pos":"LHP","score":83,"call_up":"Medium","bowman":"$10-35","topps":"$30-90"}],
-    "SEA":[{"player":"Cole Young","pos":"SS","score":87,"call_up":"High","bowman":"$20-65","topps":"$65-190"},
-           {"player":"Harry Ford","pos":"C","score":86,"call_up":"Medium","bowman":"$15-50","topps":"$50-150"},
-           {"player":"Colt Emerson","pos":"SS","score":88,"call_up":"High","bowman":"$25-80","topps":"$80-250"}],
+    "SEA":[{"player":"Colt Emerson","pos":"SS","score":88,"call_up":"High","bowman":"$25-80","topps":"$80-250"},
+           {"player":"Cole Young","pos":"SS","score":87,"call_up":"High","bowman":"$20-65","topps":"$65-190"},
+           {"player":"Harry Ford","pos":"C","score":86,"call_up":"Medium","bowman":"$15-50","topps":"$50-150"}],
     "STL":[{"player":"Masyn Winn","pos":"SS","score":88,"call_up":"High","bowman":"$20-65","topps":"$65-200"},
-           {"player":"Gordon Graceffo","pos":"RHP","score":84,"call_up":"Medium","bowman":"$10-35","topps":"$35-100"},
-           {"player":"Tink Hence","pos":"RHP","score":87,"call_up":"High","bowman":"$20-65","topps":"$65-190"}],
-    "TB": [{"player":"Jonny DeLuca","pos":"OF","score":84,"call_up":"Medium","bowman":"$10-35","topps":"$35-100"},
-           {"player":"Carson Williams","pos":"SS","score":90,"call_up":"High","bowman":"$30-90","topps":"$90-270"},
-           {"player":"Taj Bradley","pos":"RHP","score":86,"call_up":"Medium","bowman":"$15-50","topps":"$50-150"}],
+           {"player":"Tink Hence","pos":"RHP","score":87,"call_up":"High","bowman":"$20-65","topps":"$65-190"},
+           {"player":"Gordon Graceffo","pos":"RHP","score":84,"call_up":"Medium","bowman":"$10-35","topps":"$35-100"}],
+    "TB": [{"player":"Carson Williams","pos":"SS","score":90,"call_up":"High","bowman":"$30-90","topps":"$90-270"},
+           {"player":"Taj Bradley","pos":"RHP","score":86,"call_up":"Medium","bowman":"$15-50","topps":"$50-150"},
+           {"player":"Jonny DeLuca","pos":"OF","score":84,"call_up":"Medium","bowman":"$10-35","topps":"$35-100"}],
     "TEX":[{"player":"Wyatt Langford","pos":"OF","score":89,"call_up":"High","bowman":"$25-80","topps":"$80-230"},
            {"player":"Kumar Rocker","pos":"RHP","score":86,"call_up":"Medium","bowman":"$15-50","topps":"$50-150"},
            {"player":"Brock Porter","pos":"RHP","score":84,"call_up":"Medium","bowman":"$10-35","topps":"$35-100"}],
@@ -146,46 +147,35 @@ PROSPECTS = {
            {"player":"Orelvis Martinez","pos":"SS","score":85,"call_up":"Medium","bowman":"$12-40","topps":"$40-120"},
            {"player":"Leo De Vries","pos":"SS","score":85,"call_up":"Medium","bowman":"$15-50","topps":"$50-150"}],
     "WSN":[{"player":"James Wood","pos":"OF","score":96,"call_up":"High","bowman":"$80-250","topps":"$250-750"},
-           {"player":"Brady House","pos":"SS","score":86,"call_up":"Medium","bowman":"$15-50","topps":"$50-150"},
-           {"player":"Dylan Crews","pos":"OF","score":89,"call_up":"High","bowman":"$25-80","topps":"$80-230"}],
+           {"player":"Dylan Crews","pos":"OF","score":89,"call_up":"High","bowman":"$25-80","topps":"$80-230"},
+           {"player":"Brady House","pos":"SS","score":86,"call_up":"Medium","bowman":"$15-50","topps":"$50-150"}],
 }
-
-for code, _ in TEAMS:
-    if code not in PROSPECTS:
-        PROSPECTS[code] = [
-            {"player":"Jesús Made","pos":"SS","score":92,"call_up":"High","bowman":"$25-80","topps":"$80-250"},
-            {"player":"Colt Emerson","pos":"SS","score":88,"call_up":"High","bowman":"$20-65","topps":"$65-190"},
-            {"player":"Leo De Vries","pos":"SS","score":85,"call_up":"Medium","bowman":"$15-50","topps":"$50-150"},
-        ]
 
 if "current_team" not in st.session_state:
     st.session_state.current_team = None
 if "selected_player" not in st.session_state:
     st.session_state.selected_player = None
 
+# ── TEAM SELECTION ─────────────────────────────────────────────────────────
 if st.session_state.current_team is None:
     st.markdown("## ⚾ MLB Prospect Analyzer")
     st.markdown("*Trade Show Edition — Tap a team*")
 
-    grid_html = '<div style="display:grid;grid-template-columns:1fr 1fr;gap:8px;margin-top:12px;">'
+    tiles_html = ""
     for code, name in TEAMS:
         color = TEAM_COLORS.get(code, "#1c2a3e")
-        grid_html += f"""
-        <div style="
-            background:{color};
-            border-radius:10px;
-            padding:10px 12px;
-            text-align:center;
-            border:1px solid rgba(255,255,255,0.15);
-        ">
+        tiles_html += f"""<div style="background:{color};border-radius:10px;padding:10px 12px;
+            text-align:center;border:1px solid rgba(255,255,255,0.15);">
             <div style="color:#ffffff;font-weight:700;font-size:16px;">{code}</div>
             <div style="color:rgba(255,255,255,0.85);font-size:12px;">{name}</div>
-        </div>
-        """
-    grid_html += "</div>"
-    st.markdown(grid_html, unsafe_allow_html=True)
+        </div>"""
 
-    st.markdown("---")
+    components.html(f"""
+    <div style="display:grid;grid-template-columns:1fr 1fr;gap:8px;padding:4px;">
+        {tiles_html}
+    </div>
+    """, height=720, scrolling=True)
+
     st.markdown("**Select your team:**")
     cols = st.columns(2)
     for i, (code, name) in enumerate(TEAMS):
@@ -195,6 +185,7 @@ if st.session_state.current_team is None:
                 st.session_state.selected_player = None
                 st.rerun()
 
+# ── TEAM PROSPECTS ─────────────────────────────────────────────────────────
 else:
     team = st.session_state.current_team
     team_name = next(n for c, n in TEAMS if c == team)
@@ -206,13 +197,8 @@ else:
         st.rerun()
 
     st.markdown(f"""
-    <div style="
-        background:{color};
-        border-radius:12px;
-        padding:16px 20px;
-        margin:12px 0;
-        border:1px solid rgba(255,255,255,0.2);
-    ">
+    <div style="background:{color};border-radius:12px;padding:16px 20px;margin:12px 0;
+         border:1px solid rgba(255,255,255,0.2);">
         <div style="color:#ffffff;font-size:28px;font-weight:800;">{team}</div>
         <div style="color:rgba(255,255,255,0.85);font-size:16px;">{team_name} — Top Prospects</div>
     </div>
@@ -245,32 +231,29 @@ else:
 
         if st.session_state.selected_player == p["player"]:
             st.markdown(f"""
-            <div style="
-                background:#161b22;
-                border:1px solid {color};
-                border-radius:10px;
-                padding:16px 20px;
-                margin:4px 0 12px 0;
-            ">
+            <div style="background:#161b22;border:1px solid {color};border-radius:10px;
+                 padding:16px 20px;margin:4px 0 12px 0;">
                 <div style="color:#ffffff;font-size:20px;font-weight:700;margin-bottom:12px;">
-                    {p['player']} <span style="color:rgba(255,255,255,0.5);font-size:14px;">· {p['pos']}</span>
+                    {p['player']}
+                    <span style="color:rgba(255,255,255,0.5);font-size:14px;">· {p['pos']}</span>
                 </div>
                 <div style="display:grid;grid-template-columns:1fr 1fr;gap:10px;margin-bottom:14px;">
-                    <div style="background:#0d1117;border-radius:8px;padding:10px 12px;border:1px solid #30475e;">
+                    <div style="background:#0d1117;border-radius:8px;padding:10px 12px;
+                         border:1px solid #30475e;">
                         <div style="color:#8b949e;font-size:11px;margin-bottom:4px;">CALL-UP SCORE</div>
                         <div style="color:#ffffff;font-size:18px;font-weight:700;">{score}</div>
-                        <div style="background:{badge_bg};color:{badge_color};font-size:11px;font-weight:600;
-                             padding:3px 8px;border-radius:6px;display:inline-block;margin-top:4px;">
-                            {p['call_up']}
-                        </div>
+                        <div style="background:{badge_bg};color:{badge_color};font-size:11px;
+                             font-weight:600;padding:3px 8px;border-radius:6px;
+                             display:inline-block;margin-top:4px;">{p['call_up']}</div>
                     </div>
-                    <div style="background:#0d1117;border-radius:8px;padding:10px 12px;border:1px solid #30475e;">
+                    <div style="background:#0d1117;border-radius:8px;padding:10px 12px;
+                         border:1px solid #30475e;">
                         <div style="color:#8b949e;font-size:11px;margin-bottom:4px;">POSITION</div>
                         <div style="color:#ffffff;font-size:18px;font-weight:700;">{p['pos']}</div>
-                        <div style="color:#8b949e;font-size:11px;margin-top:4px;">Field position</div>
                     </div>
                 </div>
-                <div style="background:#0d1117;border-radius:8px;padding:12px 14px;border:1px solid #30475e;">
+                <div style="background:#0d1117;border-radius:8px;padding:12px 14px;
+                     border:1px solid #30475e;">
                     <div style="color:#8b949e;font-size:11px;margin-bottom:8px;">🃏 CARD VALUES</div>
                     <div style="display:flex;justify-content:space-between;margin-bottom:6px;">
                         <span style="color:#8b949e;font-size:13px;">Bowman Chrome</span>
@@ -283,5 +266,9 @@ else:
                 </div>
             </div>
             """, unsafe_allow_html=True)
+
+            if st.button("✕ Close", key=f"close_{p['player']}"):
+                st.session_state.selected_player = None
+                st.rerun()
 
 st.success(f"Updated {datetime.now().strftime('%I:%M %p')}")
